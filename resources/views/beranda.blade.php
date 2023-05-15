e93a87750be41dfdf4e16a992d2d411e42c9c252
nidanone <mnalwafi2004@gmail.com>
2023-05-15 19:59:34 +0700

    terbaru

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+
+    <link rel="stylesheet" href="css/NavigationBar.css">
+    <link rel="stylesheet" href="css/Beranda.css">
+    <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/product-sans">
+    <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/avantgarde-bk-bt-2">
+
+</head>
+<body>
+    <nav>
+        <div id="logo"></div>
+        <!-- Search Bar -->
+        <form action="" method="GET" class="search-bar">
+            <img src="img/Kaca Pembesar.png" alt="Kaca Pembesar" class="kaca-pembesar">
+            <input type="search" placeholder='Ketik "Novel Penyihir"' class="search-field"/>
+        </form>
+        <ul>
+            <li><a href="#Beranda" class="active">Beranda</a></li>
+            <li><a href="#Disimpan">Disimpan</a></li>
+            <li><a href="#Profile">Profile</a></li>
+        </ul>
+    </nav>
+    <header id="hero">
+        <div class="hero-main">
+            <img src="img/Vector Baca Buku1.png" alt="Vector Baca Buku" class="hero-image">
+            <h1>“Selama toko buku ada, selama itu pustaka bisa dibentuk kembali. Kalau perlu dan memang perlu, pakaian dan makanan dikurangi.”
+                - Tan Malaka</h1>
+        </div>
+    </header>
+    <main>
+        <section class="rekomendasi">
+            <ul>
+                <li><h1>Rekomendasi untuk kamu</h1></li>
+                <li><a href="#Lihatsemua">Lihat semua</a></li>
+            </ul>
+        </section>
+    </main>
+
+</body>
+</html>
