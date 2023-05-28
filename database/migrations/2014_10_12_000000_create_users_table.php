65ccd3c94a9cab60721bdc8d72876b7fff507f34
nidanone <mnalwafi2004@gmail.com>
2023-05-28 16:26:52 +0700

    + nomor telepon di database user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table-> string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('nomortelp')->unique();
             $table->enum('level', ['user', 'admin']);
         });
     }
